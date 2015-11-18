98250eedc79669114a37936731eba8b59eedba5c
naveen81 <naveengupta718@gmail.com>
2015-11-18 03:21:27 -0800

    initial commit

---
+++ upload_file.php
@@ -0,0 +1,38 @@
+<?php
+$allowedExts = array("pdf", "doc","docx");
+$temp = explode(".", $_FILES["file"]["name"]);
+$extension = end($temp);
+if (($_FILES["file"]["type"] == "application/pdf")
+|| ($_FILES["file"]["type"] == "application/msword")
+|| ($_FILES["file"]["type"] == "application/vnd.openxmlformats-officedocument.wordprocessingml.document")
+&& ($_FILES["file"]["size"] < 20000000)
+&& in_array($extension, $allowedExts))
+  {
+  if ($_FILES["file"]["error"] > 0)
+    {
+    echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
+    }
+  else
+    {
+    echo "Upload: " . $_FILES["file"]["name"] . "<br>";
+    echo "Type: " . $_FILES["file"]["type"] . "<br>";
+    echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+    echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
+
+    if (file_exists("upload/" . $_FILES["file"]["name"]))
+      {
+      echo $_FILES["file"]["name"] . " already exists. ";
+      }
+    else
+      {
+      move_uploaded_file($_FILES["file"]["tmp_name"],
+      "upload/" . $_FILES["file"]["name"]);
+  echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
+      }
+    }
+  }
+else
+  {
+  echo "Invalid file";
+  }
+?>
